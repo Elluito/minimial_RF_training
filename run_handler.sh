5af981eb06720d57ae3feb57d0483462458f78f9
Elluito <alfredoavendano@hotmail.com>
2024-08-15 12:58:06 +0100

    Training for 50 epochs

--- run_handler.sh
+++ run_handler.sh
@@ -5,7 +5,7 @@
 #--model --dataset --num_workers --RF_level--type --epochs  --name  --width  --record  --save_folder --data_folder
 
 # "training_Level_2_resnet50_small_imagenet"
-./run_training.sh "resnet50" "small_imagenet" 4 "2" "normal" 50 "recording_50" 1 1 $1 $2 1 &&
+./run_training.sh "resnet50" "imagenet" 4 "2" "normal" 50 "recording_50" 1 1 $1 $2 1 &&
 # "training_Level_3_resnet50_small_imagenet"
 #./run_training.sh "resnet50" "small_imagenet" 4 "3" "normal" 200 "recording_200" 1 1 $1 $2 1
 
@@ -13,7 +13,10 @@
 #./run_training.sh "resnet50" "small_imagenet" 4 "4" "normal" 200 "recording_200" 1 1 $1 $2 1
 # "training_Level_5_resnet50_small_imagenet"
 
-./run_training.sh "resnet50" "imagenet" 4 "5" "normal" 50 "recording_50" 1 1 $1 $2 1
+./run_training.sh "resnet50" "imagenet" 4 "5" "normal" 50 "recording_50" 1 1 $1 $2 1 &&
+./run_training.sh "resnet50" "imagenet" 4 "9" "normal" 50 "recording_50" 1 1 $1 $2 1 &&
+./run_training.sh "resnet50" "imagenet" 4 "9" "normal" 50 "recording_50" 1 1 $1 $2 1 &&
+./run_training.sh "resnet50" "imagenet" 4 "5" "normal" 50 "recording_50" 1 1 $1 $2 1 &&
 # "training_Level_6_resnet50_small_imagenet"
 #./run_training.sh "resnet50" "imagenet" 4 "6" "normal" 200 "recording_200" 1 1 $1 $2 1
 # "training_Level_7_resnet50_small_imagenet"
